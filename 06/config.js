c4e6b20ef438f3325ee805eb29e9929697412ed7
buivanbaodev <buivanbaodev@gmail.com>
2023-12-06 20:40:57 +0700

    add

---
+++ 06/config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'For Thu Hà ♡';
+ const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 06/01';
+const titleCard = 'Trần Thị Thu Hà';
+const contentCard = 'Chúc bae của anh 06/01 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau </br> I Love U';
+
+
+const giftImage = './06/thuha.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
